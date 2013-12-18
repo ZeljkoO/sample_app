aec5c4bb20aaff7d04ced0c6f97827ee10758673
Zeljko Obrenovic <Zeljko.Obrenovic@gmx.net>
2013-12-18 15:32:23 +0100

    Chapter 8

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,9 +25,10 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
-
-  it { should be_valid }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
+  it { should be_valid }
+
 
   describe "when name is not present" do
     before { @user.name = " " }
@@ -73,20 +74,15 @@ describe User do
       end
     end
   end
-  describe "when email address is already taken" do
-    before do
-      user_with_same_email = @user.dup
-      user_with_same_email.save
-    end
 
-    it { should_not be_valid }
-  end
   describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
       user_with_same_email.email = @user.email.upcase
       user_with_same_email.save
     end
+    it { should_not be_valid }
+  end
     describe "when password is not present" do
       before { @user.password = @user.password_confirmation = " " }
       it { should_not be_valid }
@@ -100,8 +96,6 @@ describe User do
       it { should_not be_valid }
     end
 
-    it { should_not be_valid }
-  end
   describe "with a password that's too short" do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should be_invalid }
@@ -122,5 +116,10 @@ describe User do
       specify { user_for_invalid_password.should be_false }
     end
   end
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 end
+
 
